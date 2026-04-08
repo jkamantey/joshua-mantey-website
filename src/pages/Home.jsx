818c0a526089966c83a97513c4798b5070296898
jkamantey <joshuamantey76@gmail.com>
2026-04-07 22:06:18 -0400

    Replace home portrait with professional headshot

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
           </div>
           <div className="hero-visual">
             <div className="portrait-card">
-              <img src="/images/joshuaImg.jpg" alt="Joshua Mantey" />
+              <img src="/images/Portrait.png" alt="Joshua Mantey" />
             </div>
           </div>
         </div>
